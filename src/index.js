7a5b37b1f80ad3251c7e7f41caca7f629e694523
paula <paulalaorga@gmail.com>
2023-10-13 11:31:41 +1100

    all

--- src/index.js
+++ src/index.js
@@ -1,1 +1,54 @@
-console.log('%c HI', 'color: firebrick')
+document.addEventListener("DOMContentLoaded", () => {
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+    const dogImageContainer = document.getElementById("dog-image-container");
+
+    fetch(imgUrl)
+    .then((response) => response.json())
+    .then((data) => {
+        const images = data.message;
+        images.forEach((imgUrl) => {
+            const img = document.createElement("img");
+            img.src = imgUrl;
+            dogImageContainer.appendChild(img);
+        });
+    });
+
+});
+
+const breedUrl = "https://dog.ceo/api/breeds/list/all";
+const dogBreedsList = document.getElementById("dog-breeds");
+
+fetch(breedUrl)
+.then((response) => response.json())
+.then((data) => {
+    const breeds = Object.keys(data.message);
+    breeds.forEach((breed) => {
+        const li = document.createElement("li");
+        li.textContent = breed;
+        dogBreedsList.appendChild(li);
+    });
+});
+
+dogBreedsList.addEventListener("click", (event) => {
+    if (event.target.tagName === "Li") {
+        event.target.style.color = "red";
+    }
+});
+
+const breedDropdown = document.getElementById("breed-dropdown");
+
+breedDropdown.addEventListener("change", (event) => {
+    const selectedLetter = event.target.value;
+    const breedLis = dogBreedsList.getElementsByTagName("li");
+
+    for (const breedLi of breedLis) {
+        if (breedLi.textContent.charAt(0) === selectedLetter) {
+            breedLi.style.display = "list-item";
+        } else {
+            breedLi.style.display = "none;"
+        }
+        }
+    });
+
+
+
